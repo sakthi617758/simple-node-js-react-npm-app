e16a70cf67a255768b0297192d5e5add3d7414a8
Sakthi617758 <sakthivel617758@gmail.com>
2022-05-24 19:09:12 +0530

    test

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -18,8 +18,7 @@ echo 'is followed by another command that retrieves the process ID (PID) value'
 echo 'of the previously run process (i.e. "npm start") and writes this value to'
 echo 'the file ".pidfile".'
 set -x
-npm start &
-sleep 1
+npm start 
 echo $! > .pidfile
 set +x
 
