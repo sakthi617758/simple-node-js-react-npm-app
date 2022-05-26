3040c5951e92444516041d0795dbe59ddbeda055
Sakthi617758 <sakthivel617758@gmail.com>
2022-05-26 12:04:11 +0530

    Initial commit

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -8,7 +8,7 @@ echo 'the best performance.'
 set -x
 npm run build
 echo 'copy the latest build file from jenkins workspace to web root directory of your installed web server'
-cp -r /var/lib/jenkins/workspace/Sample React Application/build /var/www/html/
+cp -r /var/lib/jenkins/workspace/SampleReactApplication/build /var/www/html/
 set +x
 
 echo 'created your initial Pipeline as a Jenkinsfile.)'
